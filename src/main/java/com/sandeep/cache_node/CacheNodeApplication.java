57fcd5abdd4e47dcf0495e0ca995d6a409a58624
hellowcoder <iitianias@gmail.com>
2026-06-01 10:00:00 +0530

    Project set up

---
+++ src/main/java/com/sandeep/cache_node/CacheNodeApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.cache_node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeep/cache_node/CacheNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.cache_node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
